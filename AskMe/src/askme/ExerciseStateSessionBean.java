9b2b6f79b9febb1c9f4ba44a775264a8ceae4f68
Vanessa <sophie.vm2189@gmail.com>
2016-05-11 12:04:48 +0100

    new driagram and erased some comments from classes

--- AskMe/src/askme/ExerciseStateSessionBean.java
+++ AskMe/src/askme/ExerciseStateSessionBean.java
@@ -2,9 +2,7 @@ package askme;
 
 import javax.ejb.Stateless;
 
-/**
- * Created by Filipa on 10/05/2016.
- */
+
 @Stateless(name = "ExerciseStateSessionEJB")
 public class ExerciseStateSessionBean {
     public ExerciseStateSessionBean() {
