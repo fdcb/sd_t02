dec85fb2266290e79cc8ac75b7751142481f920b
Vanessa <sophie.vm2189@gmail.com>
2016-05-12 20:17:11 +0100

    added pdf and project

--- AskMe/src/askme/ClassesControler.java
+++ AskMe/src/askme/ClassesControler.java
@@ -1,9 +1,27 @@
 package askme;
 
+import javax.ejb.EJB;
 import javax.faces.bean.RequestScoped;
 import javax.inject.Named;
+import java.util.ArrayList;
+import java.util.List;
 
 @Named (value="classesControler")
 @RequestScoped
 public class ClassesControler {
+
+    @EJB
+    ClassesSessionBean classesSessionBean;
+
+    List<ClassesEntity> classesList = new ArrayList<>();
+
+    public List<ClassesEntity> getProductList(){
+
+        classesList = classesSessionBean.getClasses();
+        return classesList;
+    }
+
+    ClassesEntity newClass = new ClassesEntity();
+
+    public String add
 }
--- AskMe/src/askme/UserControler.java
+++ AskMe/src/askme/UserControler.java
@@ -1,9 +1,22 @@
 package askme;
 
+import javax.ejb.EJB;
 import javax.faces.bean.RequestScoped;
 import javax.inject.Named;
+import java.util.ArrayList;
+import java.util.List;
 
 @Named (value="userControler")
 @RequestScoped
 public class UserControler {
+
+    @EJB
+    UserSessionBean user;
+
+    List<UserEntity> userList = new ArrayList<>();
+
+  /*  public List<UserEntity> getUserList(){
+        userList = user.getUsers();
+    }*/
+
 }
