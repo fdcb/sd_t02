2a01499f840ff37896fa91fa55b72e5cbc3d1cf9
Vanessa <sophie.vm2189@gmail.com>
2016-06-06 14:41:24 +0100

    Added page to consult Solutions and classify them

---
+++ Askme_v2/src/sessionBeans/UsersSessionBean.java
@@ -0,0 +1,79 @@
+package sessionBeans;
+
+import entities.Users;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.PersistenceContext;
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.List;
+
+@Stateless(name = "UsersSessionEJB")
+public class UsersSessionBean {
+
+    private EntityManagerFactory entityManagerFactory;
+
+    private EntityManagerFactory getEntityManagerFactory() {
+        HashMap<String, String> properties = new HashMap<>();
+        properties.put("ConnectionDriverName", "org.sqlite.JDBC");
+        entityManagerFactory = Persistence.createEntityManagerFactory
+                ("newpersistenceunit", properties);
+        return entityManagerFactory;
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<Users> getUsers() {
+        EntityManager entityManager = getEntityManagerFactory()
+                .createEntityManager();
+        return (List<Users>) entityManager.createNamedQuery("Users.findAll")
+                .getResultList();
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<Users> getUsers(String username) {
+        EntityManager entityManager = getEntityManagerFactory()
+                .createEntityManager();
+        return (List<Users>) entityManager.createNamedQuery("Users" +
+                ".findByUsername").setParameter("username", username)
+                .getResultList();
+    }
+
+    public void addUsers(Users users) {
+        EntityManager entityManager = getEntityManagerFactory()
+                .createEntityManager();
+        entityManager.getTransaction().begin();
+        entityManager.persist(users);
+        entityManager.getTransaction().commit();
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<Users> getUsers(String username, String password) {
+        EntityManager entityManager = getEntityManagerFactory()
+                .createEntityManager();
+        return (List<Users>) entityManager.createNamedQuery("Users" +
+                ".findByUsernamePassword").setParameter("username", username)
+                .setParameter("password", password).getResultList();
+    }
+
+    public void changeAccessNumber(Users users) {
+        EntityManager entityManager = getEntityManagerFactory()
+                .createEntityManager();
+        Users users1 = entityManager.find(Users.class, users.getUsername());
+        entityManager.getTransaction().begin();
+        users1.setNumberAccess(users1.getNumberAccess() + 1);
+        entityManager.getTransaction().commit();
+    }
+
+    public int getTotalAccess() {
+        EntityManager entityManager = getEntityManagerFactory()
+                .createEntityManager();
+        return ((int) entityManager.createNativeQuery
+                ("SELECT SUM(numberAccess) FROM Users")
+                .getSingleResult());
+    }
+
+
+}
