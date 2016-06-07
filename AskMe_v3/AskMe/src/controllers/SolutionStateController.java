96e3b105d3d46d2308844798615aebaac5b4b81e
Filipa Brito <Filipa Brito>
2016-06-07 15:04:07 +0100

    New FUNCTIONAL project :D

---
+++ AskMe_v3/AskMe/src/controllers/SolutionStateController.java
@@ -0,0 +1,130 @@
+package controllers;
+
+import entities.Classes;
+import entities.Exercise;
+import entities.Solution;
+import entities.SolutionState;
+import sessionBeans.ClassesSessionBean;
+import sessionBeans.ExerciseSessionBean;
+import sessionBeans.SolutionSessionBean;
+import sessionBeans.SolutionStateSessionBean;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+import javax.servlet.http.HttpServletRequest;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+@Named (value="solutionStateController")
+@ManagedBean
+@RequestScoped
+public class SolutionStateController implements Serializable{
+    @EJB
+    SolutionSessionBean solutionSessionBean = new SolutionSessionBean();
+    @EJB
+    ClassesSessionBean classesSessionBean = new ClassesSessionBean();
+    @EJB
+    SolutionStateSessionBean solutionStateSessionBean = new
+            SolutionStateSessionBean();
+    @EJB
+    ExerciseSessionBean exerciseSessionBean = new ExerciseSessionBean();
+
+    private String classname;
+    private String username;
+    private String solutionIdS;
+    private String exerciseId;
+    private boolean author;
+
+    private Solution solution = new Solution();
+    private Exercise exercise = new Exercise();
+
+    @PostConstruct
+    public void init(){
+        List<SolutionState> solutionStateList = solutionStateSessionBean
+                .getSolutionStates(SolutionState.UNSEEN);
+
+        int unseenId = solutionStateList.get(0).getStateId();
+
+        HttpServletRequest request = (HttpServletRequest) FacesContext.
+                getCurrentInstance().getExternalContext().getRequest();
+
+        classname = request.getParameter("classname");
+        username = request.getParameter("username");
+        exerciseId = request.getParameter("exerciseId");
+        solutionIdS = request.getParameter("solutionId");
+
+        Logger log = Logger.getLogger(ClassesController.class.getName());
+        log.info("Solution: " + solutionIdS);
+        log.info("Solution: " + exerciseId);
+        log.info("Solution: " + username);
+        log.info("Solution: " + classname);
+
+        int solutionId = Integer.parseInt(solutionIdS);
+
+        List<Classes> classesList = classesSessionBean.getClasses(classname);
+        List<Exercise> exerciseList = exerciseSessionBean.getExercises
+                (classesList.get(0).getClassId(), Integer.parseInt(exerciseId));
+        exercise = exerciseList.get(0);
+
+        List<Solution> solutionsList = solutionSessionBean.getSolutions
+                (classesList.get(0).getClassId(), exercise.getExerciseId(),
+                        solutionId);
+        solution = solutionsList.get(0);
+
+        author = exercise.getUsername().equals(username) && (solution
+                .getId_state() == unseenId);
+
+
+    }
+
+    public String getClassname(){
+        return classname;
+    }
+
+    public String getUsername(){
+        return username;
+    }
+
+    public boolean isAuthor(){
+        return author;
+    }
+
+    public Solution getSolution(){
+        return solution;
+    }
+
+    public Exercise getExercise(){
+        return exercise;
+    }
+
+    public String getExerciseId(){
+        return exerciseId;
+    }
+
+    public String getSolutionIdS(){
+        return solutionIdS;
+    }
+
+    public String changeSolutionStateWrong(){
+        List<SolutionState> solutionStateList = solutionStateSessionBean
+                .getSolutionStates(SolutionState.WRONG);
+        solutionSessionBean.changeSolutionState(solution, solutionStateList
+                .get(0).getStateId());
+        return "consult.xhtml";
+    }
+
+    public String changeSolutionStateCorrect(){
+        List<SolutionState> solutionStateList = solutionStateSessionBean
+                .getSolutionStates(SolutionState.CORRECT);
+        solutionSessionBean.changeSolutionState(solution, solutionStateList
+                .get(0).getStateId());
+        return "consult.xhtml";
+    }
+}
---
+++ AskMe_v3/AskMe/src/entities/Solution.java
@@ -0,0 +1,142 @@
+package entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Solution", schema = "", catalog = "")
+@IdClass(SolutionPK.class)
+@NamedQueries({
+        @NamedQuery(
+                name = "Solution.findAll",
+                query = "SELECT s FROM Solution s"
+        ),
+        @NamedQuery(
+                name = "Solution.findByExerciseAndClass",
+                query = "SELECT s FROM Solution s " +
+                        "WHERE s.exerciseId = :exerciseId " +
+                        "AND s.classId = :classId"
+        ),
+        @NamedQuery(
+                name = "Solution.findByExerciseClassAndSolutionId",
+                query = "SELECT s FROM Solution s " +
+                        "WHERE s.classId = :classId " +
+                        "AND s.exerciseId = :exerciseId " +
+                        "AND s.solutionId = :solutionId"
+        ),
+        @NamedQuery(
+                name = "Solution.findByState",
+                query = "SELECT s FROM Solution s " +
+                        "WHERE s.classId = :classId " +
+                        "AND s.exerciseId = :exerciseId " +
+                        "AND s.id_state = :stateId"
+        ),
+        @NamedQuery(
+                name = "Solution.findAllByState",
+                query = "SELECT s FROM Solution s " +
+                        "WHERE s.id_state = :stateId"
+        )
+})
+public class Solution {
+    private Integer solutionId;
+    private int exerciseId;
+    private int classId;
+    private String description;
+
+    @Id
+    @Column(name = "solution_id")
+    public Integer getSolutionId() {
+        return solutionId;
+    }
+
+    public void setSolutionId(Integer solutionId) {
+        this.solutionId = solutionId;
+    }
+
+    @Id
+    @Column(name = "exercise_id")
+    public int getExerciseId() {
+        return exerciseId;
+    }
+
+    public void setExerciseId(int exerciseId) {
+        this.exerciseId = exerciseId;
+    }
+
+    @Id
+    @Column(name = "class_id")
+    public int getClassId() {
+        return classId;
+    }
+
+    public void setClassId(int classId) {
+        this.classId = classId;
+    }
+
+    @Basic
+    @Column(name = "description")
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+
+        Solution that = (Solution) o;
+
+        if (exerciseId != that.exerciseId)
+            return false;
+        if (classId != that.classId)
+            return false;
+        if (solutionId != null ? !solutionId.equals(that.solutionId)
+                : that.solutionId != null)
+            return false;
+        if (description != null ? !description.equals(that.description)
+                : that.description != null)
+            return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = solutionId != null ? solutionId.hashCode() : 0;
+        result = 31 * result + exerciseId;
+        result = 31 * result + classId;
+        result = 31 * result + (description != null ? description.hashCode()
+                : 0);
+        return result;
+    }
+
+    private String username;
+
+    @Basic
+    @Column(name = "username")
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    private int id_state;
+
+    @Basic
+    @Column(name = "id_state")
+    public int getId_state() {
+        return id_state;
+    }
+
+    public void setId_state(int id_state) {
+        this.id_state = id_state;
+    }
+}
+
